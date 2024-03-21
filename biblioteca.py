973bef41bd7975863837382636dbea44f1dc4bb2
Daniele Conte <daniele@MacBook-Air-di-Daniele.local>
2024-03-21 13:13:58 +0100

    first commit

--- biblioteca.py
+++ biblioteca.py
@@ -1,2 +1,7 @@
 from flask import Flask, render_template, request, flash, url_for, session, redirect, session
+app = Flask(__name__)
 
+@app.route('/')
+def index ():
+    return render_template ('index.html')
+app.run(host='0.0.0.0', port=81, debug=True)
